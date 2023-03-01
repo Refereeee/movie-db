d5391c3728f67fefbaaf707f491951d5d79a9464
nedojuniorr <rrefereezzz@gmail.com>
2023-03-01 06:10:09 +0300

    init:movie-db

---
+++ src/context.js
@@ -0,0 +1,17 @@
+import React, {useState,useContext} from 'react'
+// make sure to use https
+import useFetch from './useFetch'
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+
+  const [query, setQuery] = useState('sup')
+  const {isLoading,error,data:movies} = useFetch(`&s=${query}`)
+  return <AppContext.Provider value={{isLoading,error,movies,query,setQuery}}>{children}</AppContext.Provider>
+}
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
